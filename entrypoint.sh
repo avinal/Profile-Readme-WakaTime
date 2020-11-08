c22ed6f81a79d0fa879372a42e4f2514b2778043
avinal <185067@nith.ac.in>
2020-11-08 12:20:54 +0530

    error detection added

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,50 +1,71 @@
 #! /bin/bash
+
+# constants
+RED='\033[0;31m' # Red color output for failed commands
+GREEN='\033[1;32m'
+clone_repo="https://github.com/${GITHUB_REPOSITORY}.git" # Repository URL
+remote_repo="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" # remote repo address
+
+# Function to check success of a command
+function check_success() {
+if [ $? -eq 0 ]
+then
+    echo -e "$1"
+else
+    echo -e "${RED}$2"
+    exit 1
+fi
+}
+
+# GITHUB_TOKEN check
 if [ -z "${INPUT_GITHUB_TOKEN}" ]; then
-    echo "error: not found GITHUB_TOKEN"
+    echo -e "${RED}error: GITHUB_TOKEN not found"
     exit 1
 fi
 
-clone_repo="https://github.com/${GITHUB_REPOSITORY}.git"
-git clone "${clone_repo}"
-echo "Repository Cloned"
+# Cloning
+git clone --single-branch --branch "${INPUT_BRANCH}" "${clone_repo}"
+check_success "cloned branch:${INPUT_BRANCH}" "error: cloning failed"
 
+# resolving repository name
 IFS='/' read -ra reponame <<< "${GITHUB_REPOSITORY}"
 repository="${reponame[1]}"
-echo "repository name resolved ${repository}"
+check_success "repository name resolved ${repository}" "error: repository name resolution failed"
 
+# chek if image is present or not
 if [[ ! -f "stat.svg" ]]; then
-    echo "error: file lost! existing"
+    echo "${RED}error: file lost! existing"
     exit 1
 fi
 
+# images folder creating/updation
 if [[ ! -d "${repository}/images" ]]
 then
     mkdir -p "${repository}/images"
-    echo "images folder created"
+    check_success "images folder created" "error: cannot create folder"
 else
     rm "${repository}/images/stat.svg" 
-    rm "${repository}/images/stat.png" 
-    echo "old images removed"
+    check_success "old images removed" "error: cannot remove images"
 fi
 
+# updating images
 cp stat.svg "${repository}/images"
-cp stat.png "${repository}/images"
-echo "copied new images"
+check_success "new image copied" "error: cannot replace image"
 
 cd "${repository}"
 
-remote_repo="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 git config user.email "41898282+github-actions[bot]@users.noreply.github.com"
 git config user.name "GitHub Actions"
-echo "git credential added" 
+check_success "git credential added" "error: git credential wrong/missing"
 
 git remote add publisher "${remote_repo}"
 git remote -v
 
-git checkout master
+git checkout ${INPUT_BRANCH}
 
-git add .
-git commit -m "Automated Coding Activity Update :alien:"
+# push to github
+git stage stat.svg
+git commit -m "${INPUT_COMMIT_MSG}"
 git pull
 git push publisher master
-echo "image update successfull"
+check_success "${GREEN}Successfully updated coding activity" "error: push error"
